499c675c61a52b891572395e1912136d517d6c7b
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-03-22 11:37:43 +0700

    remove unused model

--- gec-api/api.py
+++ gec-api/api.py
@@ -2,19 +2,13 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
 import components.gector.predict as gector
-import components.t5.predict as t5
 
 
 app = Flask(__name__)
 api = Api(app)
 
-_T5_GPU = 0
-_ROBERTA_GPU = 1
-_XLNET_GPU = 2
 
-model_gector_roberta = gector.load_for_demo(use_roberta=True, gpu_id=_ROBERTA_GPU)
-model_gector_xlnet = gector.load_for_demo(use_roberta=False, gpu_id=_XLNET_GPU)
-model_t5_large = t5.load_for_demo(gpu_id=_T5_GPU)
+model_gector_roberta = gector.load_for_demo()
 
 
 class MODEL(Resource):
@@ -27,9 +21,9 @@ class MODEL(Resource):
         if model == 'GECToR-Roberta':
             text_output_list = gector.predict_for_demo(text_input_list, model_gector_roberta)
         elif model == 'GECToR-XLNet':
-            text_output_list = gector.predict_for_demo(text_input_list, model_gector_xlnet)
+            text_output_list = "Unsupported"
         elif model == 'T5-Large':
-            text_output_list = t5.predict_for_demo(text_input_list, model_t5_large)
+            text_output_list = "Unsupported"
         else:
             raise NotImplementedError(f'Model {model} is not recognized.')        
         print(f'======OUTPUT FROM {model}=====', flush=True)
